7c51ba1880e76fc7bdcb8104aa2c104c333b1500
Mov1ux <104890041+Mov1ux@users.noreply.github.com>
2024-03-15 01:23:15 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,6 +7,38 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Contry(Base):
+    __tablename__= 'contry'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    contry =Column (Integer, ForeignKey('contry.id'))
+    contry_relationship = relationship(Contry)
+    email = Column(String(50), unique=True)
+    password = Column(String(25))
+
+class Characters(Base):
+    __tablename__= 'characters'
+    id= Column(String(20), primary_key=True)
+    name = Column(String(20),unique=True)
+    Hegiht = Column(Integer)
+    mass = Column(Integer)
+
+class Fav_Character(Base):
+    __tablename__= 'fav_character'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(20),ForeignKey('user.id'))
+    user_relationship = relationship(User)
+    character_id = Column(Integer,ForeignKey('characters.id'))
+
+
+
+
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
